aa1a3f3ca867a1cd121adbab3742e33d06fd2bc4
Michael <mvd1409@gmail.com>
2016-02-01 21:40:37 -0500

    Create main_2.asm

---
+++ LEX001/main_2.asm
@@ -0,0 +1,14 @@
+#include "p18F45K20.inc"
+
+    org	    0
+    
+    movf  0x022,0,0
+    subwf 0x020,0,0
+    movwf 0x024,0
+    movf  0x023,0,0
+    subwfb  0x021,0,0
+    movwf 0x025
+    
+    bra	    $
+    
+    end
