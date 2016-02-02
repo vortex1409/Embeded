2a7df5004efed8a7500a7006293fe7b79118d0be
Michael <mvd1409@gmail.com>
2016-02-02 12:02:27 -0500

    Update main_2.asm

--- LEX001/main_2.asm
+++ LEX001/main_2.asm
@@ -2,12 +2,12 @@
 
     org	    0
     
-    movf  0x022,0,0
-    subwf 0x020,0,0
-    movwf 0x024,0
-    movf  0x023,0,0
+    movf    0x022,0,0
+    subwf   0x020,0,0
+    movwf   0x024,0
+    movf    0x023,0,0
     subwfb  0x021,0,0
-    movwf 0x025
+    movwf   0x025
     
     bra	    $
     
