dd20f799a1fcc0bbd4204c9ecdb71165916f87b0
Michael <mvd1409@gmail.com>
2016-02-07 16:06:02 -0500

    Update q2.asm

--- Assignment_3/q2.asm
+++ Assignment_3/q2.asm
@@ -1,1 +1,4 @@
-
+movf    0x03C,0,0 ; Move contents of 3Ch to WREG
+addwf   0x03D,0,0 ; Add contents of 3Dh to WREG
+addwf   0x03E,0,0 ; Add contents of 3Eh to WREG
+movwf   0x03F,0,0 ; Take content of WREG and place it in 3Fh
