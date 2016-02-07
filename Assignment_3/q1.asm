2cb6090b1820aee62a2aec1cdb0beac6d5c436fc
Michael <mvd1409@gmail.com>
2016-02-07 13:22:11 -0500

    Update q1.asm

--- Assignment_3/q1.asm
+++ Assignment_3/q1.asm
@@ -1,11 +1,11 @@
 movlw 0x012 ;Load 12 into WREG
 
-movwf 0x02C,0,0 ;Move WREG to REG 02C
+addwf 0x02C,0,0 ;Move WREG to REG 02C
 
 movlw 0x012 ;Load 12 into WREG
 
-movwf 0x02E,0,0 ;Move WREG to REG 02E
+addwf 0x02E,0,0 ;Move WREG to REG 02E
 
 movlw 0x012 ;Load 12 into WREG
 
-movwf 0x03C,0,0 ;Move WREG to REG 03C
+addwf 0x03C,0,0 ;Move WREG to REG 03C
