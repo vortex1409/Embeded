898e3ab3ee14f63889d16345d2ffa3e6ad12cc51
Michael <mvd1409@gmail.com>
2016-02-07 15:57:55 -0500

    Update q1.asm

--- Assignment_3/q1.asm
+++ Assignment_3/q1.asm
@@ -1,11 +1,4 @@
-movlw 0x012
-
-addwf 0x02C,0,0 
-
-movlw 0x012 
-
-addwf 0x02E,0,0 
-
-movlw 0x012
-
-addwf 0x03C,0,0 
+movlw 0x012 ;Load 12h into WREG
+addwf 0x02C,0,0 ;Add WREG to 2Ch
+addwf 0x02E,0,0 ;Add WREG to 2Eh
+addwf 0x03C,0,0 ;Add WREG to 3Ch
