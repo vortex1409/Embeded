35312079fc160707629ea1afc1d2a8a972d0adf1
Michael <mvd1409@gmail.com>
2016-02-14 16:37:24 -0500

    Update q2.asm

--- Assignment_4/q2.asm
+++ Assignment_4/q2.asm
@@ -1,3 +1,8 @@
+; Subtracts the contents of WREG from f
 subwf   0x03E,1,0
+
+; Subtracks contents of WREG from f with borrow
 subfwb  0x02D,0,0
+
+; Subtracts the contents of WREG from f
 subwf   0x01A,1,0
