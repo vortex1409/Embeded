49661f4d78a5e384444405c2c9ef89e087e83e21
Michael <mvd1409@gmail.com>
2016-02-14 15:58:10 -0500

    Update q1.asm

--- Assignment_4/q1.asm
+++ Assignment_4/q1.asm
@@ -1,4 +1,11 @@
+; Moves 5 to Working Register
 movlw   0x5
+
+; Subtract contents of WREG from f 0x03C
 subwf   0x03C,1,0
+
+; Subtracks contents of WREG from f 0x030
 subwf   0x030,1,0
+
+; Subtracts contents of WREG from f 0x03E
 subwf   0x03E,1,0
