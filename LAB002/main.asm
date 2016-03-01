0fffd67b8c8d4967819b57b54ac0c186c3d0db05
Michael <mvd1409@gmail.com>
2016-03-01 09:46:24 -0500

    Update main.asm

--- LAB002/main.asm
+++ LAB002/main.asm
@@ -1,8 +1,8 @@
 #include "p18f45k20.inc"
 
-Status_Temp	EQU 0x20
-BSR_Temp	EQU 0x021
-WREG_Temp	EQU 0x022
+STATUS_TEMP	EQU 0x20 ; Equate STATUS_TEMP to 20h
+BSR_TEMP	EQU 0x021 ; Equate BSR_TEMP to 21h
+WREG_TEMP	EQU 0x022 ; Equate WREG_TEMP to 22h
 	
 	org 0x0000
 	goto GLEP
@@ -19,15 +19,20 @@ Loop:
 	bra Loop
 	
 Initalize:
-	bsf	OSCON, IDLEN, A
+	bsf	OSCCON, IDLEN, A
+	;bcf	OSCCON, IDLEN, A ; Part 4
 	clrf	LATD, A
 	clrf	TRISD, A
 	
 	bsf	TRISB, RD0, A
-	bcf	INTCON2, INTEDG0, A
+	bcf	INTCON2, INTEDG0, A ; Rising Edge Trigger
+	;bcf	INTCON2, INTEDG1, A ;Part 3 Falling Edge Trigger
 	bcf	INTCON, INT0IE, A
 	
-	movlw	B'10000000'
+	movlw	B'10000010' ; 2 Second On/Off pulse
+	;movlw   B'10000000' ;Faster Hearbeat
+	;movlw   B'10001000' ;Quarter Second
+	;movlw   B'10000011' ;4.19 seconds
 	movwf	T0CON, A
 	bsf	INTCON2, TMR0IP, A
 	bsf	INTCON, TMR0IE, A
@@ -58,14 +63,14 @@ HPIEP:
 LPIEP:
     retfie
     
-TImer0ISR:
+Timer0ISR:
     bcf		INTCON, TMR0IF, A
     call	Heartbeat
     
     return
     
 Int0ISR:
-    bcf		INTCON, Int0IF, A
+    bcf		INTCON, INT0IF, A
     call	UpdateStatus
     
     return
